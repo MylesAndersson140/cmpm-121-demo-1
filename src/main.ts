84416b9ec68f2d8247513954d62315298356a201
MylesAndersson140 <134179125+MylesAndersson140@users.noreply.github.com>
2024-10-14 02:34:39 -0700

    Step 4 Complete

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,11 @@ const clickCounterDiv = document.createElement("div");
 clickCounterDiv.id = "click-counter";
 let clickCounter = 0;
 
+//Setting up variables for animation
+//Undefined for the very first frame such that the counter can start
+let prevTimestamp: number | undefined;
+const incrementPerSecond = 1;
+
 //Function to help us update the click counter.
 function updateClickCounter() {
   clickCounterDiv.textContent = `${clickCounter} ${clickCounter === 1 ? "Apple" : "Apples"}`;
@@ -28,16 +33,28 @@ button.addEventListener("click", () => {
   updateClickCounter();
 });
 
-//Function to increment the counter automatically
-function autoIncrement() {
-  clickCounter++;
+//Function for a better counter
+function betterCounter(currTimestamp: number) {
+  if (prevTimestamp === undefined) {
+    prevTimestamp = currTimestamp;
+  }
+
+  const elapsed = currTimestamp - prevTimestamp;
+  prevTimestamp = currTimestamp;
+
+  //Calculate fractional increase
+  //Rounding the fractional increase as the continous growth was initally unwieldly to see
+  const fracIncrease =
+    Math.round(((incrementPerSecond * elapsed) / 1000) * 100) / 100;
+  clickCounter = Math.round((clickCounter + fracIncrease) * 100) / 100;
+
   updateClickCounter();
+
+  requestAnimationFrame(betterCounter);
 }
 
-//Using the setInterval function to increment automatically
-setInterval(autoIncrement, 1000); //1000 milliseconds / 1 second
+requestAnimationFrame(betterCounter);
 
 //Inital set up
-updateClickCounter();
 app.append(button);
 app.append(clickCounterDiv);
