8881105a8acd7c66d581fde4ae9f57feb4f4575d
MylesAndersson140 <134179125+MylesAndersson140@users.noreply.github.com>
2024-10-13 22:39:04 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.textContent = "🍎";
+button.addEventListener("click", () => {
+  //something happens when clicked!
+});
+
+app.append(button);
